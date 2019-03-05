35dd36cf00b403dbe7f9d6178006f5ed27ecc544
TextError <savinraul1983@yahoo.com>
2019-03-05 20:10:15 +0000

    redux

--- src/App.js
+++ src/App.js
@@ -7,30 +7,44 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Login from './components/authentication/Login/Login';
 import Register from './components/authentication/register/Register';
+import Dashboard from './components/dashboard/Dashboard';
+
+// Redux
+import store from './store';
+import { Provider } from 'react-redux';
 
 // Layout
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 
+// Common
+import PrivateRoute from './components/common/privateRoute';
+
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Router>
-            <div className='container-fluid'>
-              <Navbar />
-              <Route exact path='/home' component={Home} />
-              <Route exact path='/about' component={About} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/register' component={Register} />
-              
-              <Footer />
-            </div>
-          </Router>
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Router>
+              <div className='container-fluid'>
+                <Navbar />
+                <Route exact path='/home' component={Home} />
+                <Route exact path='/about' component={About} />
+                <Route exact path='/login' component={Login} />
+                <Route exact path='/register' component={Register} />
+
+                <Switch>
+                  <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                </Switch>
+                
+                <Footer />
+              </div>
+            </Router>
+          </Switch>
+        </Router>
+      </Provider>
     );
   }
 }
