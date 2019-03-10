1877590e095f62d3989c6569f6c568e35db87e94
TextError <savinraul1983@yahoo.com>
2019-03-10 09:48:55 +0000

    simple footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default class Footer extends Component {
   render() {
     return (
-      <div>
-        footer
+      <div className='footer'>
+        <div className='row'>
+          <div className='col-6'>
+            <div className='text-center'>
+              <span>Copyright &copy; {new Date().getFullYear()}</span>
+            </div>
+          </div>
+          <div className='col-6'>
+            <div>
+              <span className='ml-2 mr-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='m-2 fab fa-facebook-f fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='ml-2 mr-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='m-2 fab fa-twitter fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='ml-2 mr-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='m-2 fab fa-instagram fa-1x'></i>
+                </NavLink>
+              </span>
+              <span className='ml-2 mr-2'>
+                <NavLink to={'/'} target='_blank'>
+                  <i className='m-2 fab fa-linkedin-in fa-1x'></i>
+                </NavLink>
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
