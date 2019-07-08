13636ac71d2d4a21f1afc862abb8ee39dcad41bc
TextError <savinraul1983@yahoo.com>
2019-07-08 16:58:20 +0100

    logo redirect to home

--- src/components/layout/Logo.js
+++ src/components/layout/Logo.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 export default function Logo() {
   return (
     <div className='logo'>
-      <img src={logo} alt='logo' />
+      <Link to='/'><img src={logo} alt='logo' /></Link>
     </div>
   )
 }
